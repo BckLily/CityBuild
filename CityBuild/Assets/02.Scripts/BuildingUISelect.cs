58f786cfcd09d80d5e5819a1379fc1d4b325246d
Lilypy <52183009+BckLily@users.noreply.github.com>
2021-06-21 17:57:01 +0900

    CityBuildUI
    Making UI Test
    Build List UI / building List UI
    Click / Open / Close

---
+++ CityBuild/Assets/02.Scripts/BuildingUISelect.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingUISelect : MonoBehaviour
+{
+    Button[] buttons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttons = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
